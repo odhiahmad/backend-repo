52f837c7335c63ee4425a469630e76cd8e14c108
Odhi Ahmad <odhiahmad15@gmail.com>
2024-12-26 11:35:29 +0700

    :wrench: adding service config

--- config/firebaseConfig.ts
+++ config/firebaseConfig.ts
@@ -3,7 +3,7 @@ import * as admin from 'firebase-admin';
 const serviceAccount = require('./serviceAccount.json');
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-//   databaseURL: 'https://<your-database-name>.firebaseio.com'
+  databaseURL: 'https://backend-repo-f4fe1.firebaseio.com'
 });
 
 export default admin;
